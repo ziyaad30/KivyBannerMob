787601fe6c53a34a2107e1665caf9e5ef220efbf
Ziyaad <xavier.baatjes@outlook.com>
2021-04-06 09:55:22 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='KivyBannerMob',
+      version='1.0',
+      description='The funniest joke in the world',
+      url='http://github.com/ziyaad30/KivyBannerMob',
+      author='Ziyaad30',
+      author_email='',
+      license='MIT',
+      packages=['KivyBannerMob'],
+      zip_safe=False)
